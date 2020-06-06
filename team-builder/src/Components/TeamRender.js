bce2005d2f3a8e4fdaac56863e5a558b1b7e18a8
Freet8707 <freet8707@gmail.com>
2020-06-05 22:13:04 -0500

    go back button added inside of TeamRender.js in order to remove render of component, keeping state of teamMembers array

--- team-builder/src/Components/TeamRender.js
+++ team-builder/src/Components/TeamRender.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function TeamDisplay(props) {
     const teamMembers = props.teamMembers
+    const history = useHistory()
 
     function MemberCard(props){
         return (
@@ -17,6 +19,7 @@ function TeamDisplay(props) {
 
     return(
         <div>
+            <button onClick={() => history.push('/')}>Go Back</button>
             {teamMembers.map(member => (
                 <MemberCard key={member.id} name={member.teamMember.name} email={member.teamMember.email} role={member.teamMember.role} />     
             ))}
