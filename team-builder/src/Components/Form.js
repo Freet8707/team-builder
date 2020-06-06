36278d01b00c4344c87f0892b81b02d348570edd
Freet8707 <freet8707@gmail.com>
2020-06-05 20:39:39 -0500

    form infrastructure complete in form.js

--- team-builder/src/Components/Form.js
+++ team-builder/src/Components/Form.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = (props) => {
-    console.log(props)
+    const [teamMember, setTeamMember] = useState({name: 'Enter Name', email: 'Enter Email', role: 'Enter Team Member Role'})
+    const spaceSetter = ' '
     return(
         <div>
-            Inside the Form Component
+            <h2>Enter new team member information!</h2>
+            <form>
+                <label htmlFor='nameInput'>Name:{spaceSetter}
+                    <input type='text' id='nameInput' name='name' value={teamMember.name} />
+                </label><br />
+                <label htmlFor='emailInput'>Email:{spaceSetter}
+                    <input type='text' id='emailInput' name='email' value={teamMember.email} />                    
+                </label><br />
+                <label htmlFor='roleInput'>Role:{spaceSetter}
+                    <input type='text' id='roleInput' name='role' value={teamMember.role} />                     
+                </label>
+            </form>
         </div>
     )
 }
