58bcf7e91ee40ae5228e1049bd6507ba169a99f6
Freet8707 <freet8707@gmail.com>
2020-06-05 20:51:17 -0500

    Form functionalit confirmed in form.js, inputs update associated name keys in state variable 'team member'

--- team-builder/src/Components/Form.js
+++ team-builder/src/Components/Form.js
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Form = (props) => {
     const [teamMember, setTeamMember] = useState({name: 'Enter Name', email: 'Enter Email', role: 'Enter Team Member Role'})
     const spaceSetter = ' '
+    
+    const addNewMember = props.setTeamMembers()
+
+    const handleChanges = event => {
+        setTeamMember({...teamMember, [event.target.name]: event.target.value})
+    }
+    
+    useEffect(() => {
+        console.log(teamMember)
+    }, [teamMember])
+    
     return(
         <div>
             <h2>Enter new team member information!</h2>
             <form>
                 <label htmlFor='nameInput'>Name:{spaceSetter}
-                    <input type='text' id='nameInput' name='name' value={teamMember.name} />
+                    <input type='text' id='nameInput' name='name' value={teamMember.name} onChange={handleChanges}/>
                 </label><br />
                 <label htmlFor='emailInput'>Email:{spaceSetter}
-                    <input type='text' id='emailInput' name='email' value={teamMember.email} />                    
+                    <input type='text' id='emailInput' name='email' value={teamMember.email} onChange={handleChanges}/>                    
                 </label><br />
                 <label htmlFor='roleInput'>Role:{spaceSetter}
-                    <input type='text' id='roleInput' name='role' value={teamMember.role} />                     
+                    <input type='text' id='roleInput' name='role' value={teamMember.role} onChange={handleChanges}/>                     
                 </label>
             </form>
         </div>
