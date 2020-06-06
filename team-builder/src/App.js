cccfe4d0acc4648594d7d1f9a7abf9f57269e665
Freet8707 <freet8707@gmail.com>
2020-06-05 20:26:33 -0500

    basic framework created for project including the form.js component rendering within the app component

---
+++ team-builder/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Form from './Components/Form'
+import './App.css';
+
+function App() {
+  const [teamMembers, setTeamMembers] = useState([])
+  
+  return (
+    <div className="App">
+      <Form teamMembers={teamMembers} setTeamMembers={setTeamMembers} />
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/Components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form = (props) => {
+    console.log(props)
+    return(
+        <div>
+            Inside the Form Component
+        </div>
+    )
+}
+
+export default Form
